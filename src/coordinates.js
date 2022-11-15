70a49d11fbe8c845cda90abb8b6a911879ec1e73
Edpow <e_dg_roch@hotmail.com>
2022-11-15 11:02:02 -0400

    Initial commit

---
+++ src/coordinates.js
@@ -0,0 +1,8 @@
+import transformation  from 'transform-coordinates';
+
+export function ConvertCoordinates(line){
+    const transform  = transformation('EPSG:31981', 'EPSG:4326')
+    const result = transform.forward({x: parseFloat(line[0]), y: parseFloat(line[1])})
+    const {x, y} = result
+    return ([x, y].join(";"))
+}
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './css/style.css'
+import { ConvertCoordinates } from './coordinates.js';
+
+const inputText = document.getElementById('inputText');
+const outputText = document.getElementById('outputText');
+const button = document.getElementById('btConverter');
+
+button.addEventListener("click",() => {ReadTextFromInput()})
+
+function ReadTextFromInput() {
+    const lines = inputText.value.split(/\r?\n/);
+    const splittedLines = lines.map(splitLine);
+    const convertedLines = splittedLines.map((line) => ConvertCoordinates(line));
+    outputText.value = convertedLines.join("\n");
+}
+
+function splitLine(line) {
+    return(line.split(';'));
+}
